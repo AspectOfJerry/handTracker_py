5951543c272822bff02a9815a8dcd6028842a663
Jerry <jerryfu_mc@outlook.com>
2023-05-20 14:01:33 -0400

    update

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # handTracker-py
-Simple hand tracker using MediaPipe and OpenCV
+
+A simple hand tracker using MediaPipe and OpenCV
+
+## Usage
+
+### Including repo
+
+To add the repo in another repo as submodule, run the following command
+
+```shell
+git submodule add https://github.com/AspectOfJerry/handTracker_py.git
+```
+
+### Installing dependencies
+
+Run the following command in the root folder of the project:
+
+```shell
+pip install -r requirements.txt
+```
+
+### Importing
+
+```python
+from handTracker_py.handTracker import handTracker
+```
+
+### Documentation
+
+#### handDetector()
+
+```python
+# leave `mode` True for video streams (false for stream of unrelated images), maxHands, detectionConfidence, trackingConfidence
+def __init__(self, mode=True, maxHands=2, detectionConfidence=0.55, trackingConfidence=0.5)
+```
+
+Invoke it like this:
+
+```python
+tracker = handDetector()
+```
+
+#### findHands()
+
+```python
+# image, draw on the image?
+def findHands(self, image, draw=True)
+# returns new image
+```
+
+#### getPos()
+
+```python
+# image, hand number, draw on the image?
+def getPos(self, image, handN=0, draw=True)
+# returns array of the landmarks
+```
+
+Put the result in an array and access each landmark by index. Refer to the image below for more detail on each id.
+
+### Landmarks
+
+![Landmarks](./mediapipe_hand_landmarks.png)
--- handTracker.py
+++ handTracker.py
@@ -2,8 +2,9 @@ import cv2
 import mediapipe
 
 
-class handDetector():
-    def __init__(self, mode=False, maxHands=2, detectionConfidence=0.55, trackingConfidence=0.5):
+class handTracker():
+    # leave `mode` True for video streams (false for stream of unrelated images), maxHands, detectionConfidence, trackingConfidence
+    def __init__(self, mode=True, maxHands=2, detectionConfidence=0.60, trackingConfidence=0.5):
         self.mode = mode
         self.maxHands = maxHands
         self.detectionConfidence = detectionConfidence
@@ -13,6 +14,7 @@ class handDetector():
         self.hands = self.mpHands.Hands(self.mode, self.maxHands, 1,  self.detectionConfidence, self.trackingConfidence)
         self.mediapipeDraw = mediapipe.solutions.drawing_utils
 
+    # image, draw on the image?
     def findHands(self, image, draw=True):
         imageRGB = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         self.results = self.hands.process(imageRGB)
@@ -20,11 +22,12 @@ class handDetector():
         if (self.results.multi_hand_landmarks):
             for handLandmarks in self.results.multi_hand_landmarks:
                 if (draw):
-                    cv2.putText(image, "Confidence: " + str(self.results.multi_handedness[0].classification[0].score) or "ERR", (20, 150), cv2.FONT_HERSHEY_PLAIN, 1.5, (253, 253, 253), 1)
+                    cv2.putText(image, f"Confidence: {str(round(self.results.multi_handedness[0].classification[0].score, 3))}", (10, 25), cv2.FONT_HERSHEY_PLAIN, 1.5, (253, 253, 253), 1)
                     self.mediapipeDraw.draw_landmarks(image, handLandmarks, self.mpHands.HAND_CONNECTIONS)
 
         return image
 
+    # image, hand number, draw on the image?
     def getPos(self, image, handN=0, draw=True):
         landmarks = []
 
@@ -34,9 +37,9 @@ class handDetector():
             for id, landmark in enumerate(hand.landmark):
                 height, width, channel = image.shape
                 cx, cy = int(landmark.x * width), int(landmark.y * height)
-                # print(id, cx, cy)
+
                 landmarks.append([id, cx, cy])
                 if (draw):
-                    cv2.circle(image, (cx, cy), 5, (0, 255, 0), cv2.FILLED)  # img, center, radius, color, thickness
+                    cv2.circle(image, (cx, cy), 5, (0, 255, 0), 1)  # img, center, radius, color, thickness
 
         return landmarks
